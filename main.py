1150b4072ef1969083c754da2bba4b558a364dfa
Davi Souza <davi.souza@upperds.com>
2022-03-20 10:57:12 -0300

    ajustando for e while

--- main.py
+++ main.py
@@ -22,36 +22,39 @@ def search_tweets(q, count=100, result_type="recent"):
 
 
 def send_message(message, q, count, postgres_connection, result_type='recent'):
-    result = search_tweets(q, USERS_PER_ROUND, result_type)
-    sended_users = postgres_connection.get_all_user_already_in_tag(TAG)
-
-    print(f"searched results: {len(result['statuses'])}")
-
     count_sended = 0
-    for tweet in result["statuses"]:
-        message_to_send = f"{message}"
-        if int(tweet["user"]["id"]) not in sended_users:
-            try:
-                t.direct_messages.events.new(
-                _json={
-                    "event": {
-                        "type": "message_create",
-                        "message_create": {
-                            "target": {
-                                "recipient_id": tweet["user"]["id"]},
-                            "message_data": {
-                                "text": message_to_send}}}})
-                print(f"Mensagem enviada para {tweet['user']['name']}.")
-                postgres_connection.insert_user_in_table(tweet["user"]["id"], q, message_to_send)
-                sended_users = postgres_connection.get_all_user_already_in_tag(TAG)
-                count_sended += 1
-                seconds = random.randint(70, MAX_SECCONDS)
-                print(f"Aguardando {seconds} segundos para o próximo envio.")
-                time.sleep(seconds)
-            except Exception as ex:
-                postgres_connection.insert_user_in_table(tweet["user"]["id"], q, message_to_send, False)
-            if count_sended == count:
-                break
+    while True:
+        result = search_tweets(q, USERS_PER_ROUND, result_type)
+        sended_users = postgres_connection.get_all_user_already_in_tag(TAG)
+
+        print(f"searched results: {len(result['statuses'])}")
+        if count_sended == count:
+            break
+
+        for tweet in result["statuses"]:
+            message_to_send = f"{message}"
+            if int(tweet["user"]["id"]) not in sended_users:
+                try:
+                    t.direct_messages.events.new(
+                    _json={
+                        "event": {
+                            "type": "message_create",
+                            "message_create": {
+                                "target": {
+                                    "recipient_id": tweet["user"]["id"]},
+                                "message_data": {
+                                    "text": message_to_send}}}})
+                    print(f"Mensagem enviada para {tweet['user']['name']}.")
+                    postgres_connection.insert_user_in_table(tweet["user"]["id"], q, message_to_send)
+                    sended_users = postgres_connection.get_all_user_already_in_tag(TAG)
+                    count_sended += 1
+                    seconds = random.randint(70, MAX_SECCONDS)
+                    print(f"Aguardando {seconds} segundos para o próximo envio.")
+                    time.sleep(seconds)
+                except Exception:
+                    postgres_connection.insert_user_in_table(tweet["user"]["id"], q, message_to_send, False)
+                if count_sended == count:
+                    break
 
 try:
     postgres_connection = PostgresConnection()
